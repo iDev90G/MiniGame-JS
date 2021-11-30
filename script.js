26d81f916f6bbd01a30bf2bddd02c5ebef7fe860
Abraham R <71684482+iDev90G@users.noreply.github.com>
2021-11-30 14:52:29 -0500

    update script.js

--- script.js
+++ script.js
@@ -46,7 +46,11 @@ for (let i = 0; i < brickRowCount; i++) {
   for (let j = 0; j < brickColumnCount; j++) {
     const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
     const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
-    bricks[i][j] = { x, y, ...brickInfo };
+    bricks[i][j] = {
+      x,
+      y,
+      ...brickInfo
+    };
   }
 }
 
@@ -80,7 +84,7 @@ function drawBricks() {
     column.forEach(brick => {
       ctx.beginPath();
       ctx.rect(brick.x, brick.y, brick.w, brick.h);
-      ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+      ctx.fillStyle = brick.visible ? '#0095dd': 'transparent';
       ctx.fill();
       ctx.closePath();
     });
